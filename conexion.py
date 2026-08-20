23f022629836368a4c7d439bf6e2a76eee9386c6
Andres Forero <163767388+Dvskked@users.noreply.github.com>
2026-08-19 21:51:49 -0500

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -1,5 +1,16 @@
 import mysql.connector
+from mysql.connector import Error
 
 def obtener_conexion():
-    # Devuelve None para evitar errores de conexión local en el servidor
-    return None
+    try:
+        conexion = mysql.connector.connect(
+            host="b8y5mp8b50lm12bqe1og-mysql.services.clever-cloud.com",
+            user="umjhpxyzaojbzblh", # El usuario que te muestra Clever Cloud en la pestaña Information
+            password="0O3mCa7Ykc3BcQow97Y4", # La contraseña que te muestra Clever Cloud en la pestaña Information
+            database="b8y5mp8b50lm12bqe1og",
+            port=3306
+        )
+        return conexion
+    except Error as e:
+        print(f"Error al conectar con MySQL: {e}")
+        return None
