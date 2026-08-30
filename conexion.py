d79a96533b389698ad8c4feadb299142fd5db7d9
Andres Forero <163767388+Dvskked@users.noreply.github.com>
2026-08-30 15:13:30 -0500

    Mask database connection credentials
    Updated database connection credentials to be more secure by masking sensitive information.

--- conexion.py
+++ conexion.py
@@ -3,13 +3,16 @@ from mysql.connector import Error
 
 def obtener_conexion():
     try:
-        conexion = mysql.connector.connect(
-            host="b8y5mp8b50lm12bqe1og-mysql.services.clever-cloud.com",
-            user="umjhpxyzaojbzblh", # El usuario que te muestra Clever Cloud en la pestaña Information
-            password="0O3mCa7Ykc3BcQow97Y4", # La contraseña que te muestra Clever Cloud en la pestaña Information
-            database="b8y5mp8b50lm12bqe1og",
-            port=3306
+        conexion = mysql.connector.connect(  #Las credenciales de la base de datos las tengo solamente yo, entonces no es posible hace la conexion con sigirec Origins
+            host="####################",
+            user="###########################", 
+            password="################", 
+            database="##################",
+            port=######
         )
+
+
+        #
         return conexion
     except Error as e:
         print(f"Error al conectar con MySQL: {e}")
